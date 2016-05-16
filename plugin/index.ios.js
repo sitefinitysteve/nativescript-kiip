8609f61773a72adea9977fe11b73ad1c09a782f6
Steve McNiven-Scott <steve@sitefinitysteve.com>
2016-05-16 15:01:26 -0400

    Header search paths

--- plugin/index.ios.js
+++ plugin/index.ios.js
@@ -1,8 +1,48 @@
+var app = require("application");
 var settings = {
-
+    testMode: false
 };
 
 exports.initalize = function (options) {
     debugger;
     var kiip = Kiip.alloc();
+}
+
+exports.instance = function () {
+    //TODO
+}
+
+exports.setEmail = function (email) {
+    //TODO
+}
+
+exports.setGender = function (gender) {
+    //TODO
+}
+
+exports.getDeviceIdentifier = function () {
+    //TODO
+}
+
+exports.getCapabilities = function () {
+    //TODO
+}
+/*
+exports.setBirthday = function (birthday) {
+    debugger;
+    var format = new java.lang.SimpleDateFormat("dd/mm/yy", Locale.ENGLISH);
+    var date = format.parse(birthday);
+    me.kiip.sdk.Kiip.getInstance().setBirthday(date);
+}
+*/
+exports.saveMoment = function (options) {
+        //TODO
+}
+
+exports.startSession = function () {
+        //TODO
+}
+
+exports.endSession = function () {
+        //TODO
 }
